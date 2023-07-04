e17e8638b256289511b9f8ed8ccaac75c939de3d
@SIA <59891156+SA-Tester@users.noreply.github.com>
2023-07-04 22:28:51 +0530

    Implementing the Audio Player
    New css and js files were added for more clarity.
    The free version indicator box is commented out as it takes up the general position for the audio player.

---
+++ scripts/audio-player.js
@@ -0,0 +1,155 @@
+//let now_playing = document.querySelector(".now-playing");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+
+let playpause_btn = document.querySelector(".play-pause-track");
+let prev_btn = document.querySelector(".prev-track");
+let next_btn = document.querySelector(".next-track");
+
+let seek_slider = document.querySelector(".seek-slider");
+let volume_slider = document.querySelector(".volume-slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+
+let track_index = 0;
+let isPlaying = false;
+let updateTimer;
+
+let curr_track = document.createElement('audio');
+
+/*let track_list = [
+    {
+        name: "Gave It All Away",
+        artist: "Boyzone",
+        image: "../images/Gave_It_All_Away.jpg",
+        path: "../songs/Gave It All Away.mp3"
+    },
+    {
+        name: "Castle On the Hill",
+        artist: "Ed Sheeran",
+        image: "../images/Divide.jpg",
+        path: "../songs/Castle on the Hill.mp3"
+    },
+    {
+        name: "2002",
+        artist: "Anne Marie",
+        image: "../images/2002.jpg",
+        path: "../songs/2002.mp3"
+    },
+];*/
+
+let track_list = [
+    {
+      name: "Night Owl",
+      artist: "Broke For Free",
+      image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+      path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
+    },
+    {
+      name: "Enthusiast",
+      artist: "Tours",
+      image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+      path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3"
+    },
+    {
+      name: "Shipping Lanes",
+      artist: "Chad Crouch",
+      image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+      path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3",
+    },
+  ];
+
+function loadTrack(track_index){
+    clearInterval(updateTimer);
+    resetValues();
+
+    curr_track.src = track_list[track_index].path;
+    curr_track.load();
+
+    track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+    track_name.textContent = track_list[track_index].name;
+    track_artist.textContent = track_list[track_index].artist;
+    //now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
+
+    updateTimer = setInterval(seekUpdate, 1000); //// Set an interval of 1000 milliseconds for updating the seek slider
+    curr_track.addEventListener("ended", nextTrack);
+}
+
+function resetValues(){
+    curr_time.textContent = "00:00";
+    total_duration.textContent = "00:00";
+    //seek_slider.value = 0;
+}
+
+loadTrack(track_index);
+
+function playPauseTrack(){
+    if(!isPlaying) playTrack();
+    else pauseTrack();
+}
+
+function playTrack(){
+    curr_track.play();
+    isPlaying = true;
+
+    playpause_btn.innerHTML = "<i class='fa fa-pause-circle fa-5x'></i>";
+}
+
+function pauseTrack(){
+    curr_track.pause();
+    isPlaying = false;
+
+    playpause_btn.innerHTML = "<i class='fa fa-play-circle fa-5x'></i>";
+}
+
+function nextTrack(){
+    if(track_index < track_list.length - 1){
+        track_index += 1;
+    }
+    else track_index = 0;
+
+    loadTrack(track_index);
+    playTrack();
+}
+
+function prevTrack(){
+    if(track_index > 0){
+        track_index -= 1;
+    }
+    else track_index = track_list.length - 1;
+
+    loadTrack(track_index);
+    playTrack();
+}
+
+function seekTo(){
+    let seekto = curr_track.duration * (seek_slider.value / 100);
+    curr_track.currentTime = seekto;
+}
+
+function setVolume(){
+    curr_track.volume = volume_slider.value/100;
+}
+
+function seekUpdate(){
+    let seekPosition = 0;
+
+    if(!isNaN(curr_track.duration)){
+        seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+        seek_slider.value = seekPosition;
+
+        let currentMinutes = Math.floor(curr_track.currentTime/60);
+        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes*60);
+        let durationMinutes = Math.floor(curr_track.duration/60);
+        let durationSeconds = Math.floor(curr_track.duration - durationMinutes*60);
+
+        if(currentSeconds < 10) currentSeconds = "0" + currentSeconds;
+        if(durationSeconds < 10) durationSeconds = "0" + durationSeconds;
+        if(currentMinutes < 10) currentMinutes = "0" + currentMinutes;
+        if(durationMinutes < 10) durationMinutes = "0" + durationMinutes;
+
+        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
+    }
+}
