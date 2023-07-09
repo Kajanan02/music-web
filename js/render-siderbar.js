1fa8dcddd8aa77c429a721dcb9f505c00c820b59
@SIA <59891156+SA-Tester@users.noreply.github.com>
2023-07-09 21:04:41 +0530

    Restructuring Files by moving out reused code as templates
    Additional: the name hipsound was changed to melomaniac.

---
+++ js/render-siderbar.js
@@ -0,0 +1,75 @@
+const sidebar = document.createElement("template");
+sidebar.innerHTML = `
+    <div class="sidebar">
+        <div class="logo">
+            <a href="#">
+                <img src="assets/logos/melomaniac.png" alt="Logo" />
+            </a>
+        </div>
+
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="index.html">
+                        <span class="fa fa-fw fa-home"></span>
+                        <span>Home</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="user-artists.html">
+                        <span class="fa fa-fw fa-users"></span>
+                        <span>Artists</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="user-albums.html">
+                        <span class="fa fa-fw fa-play"></span>
+                        <span>Albums</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span class="fa fa-fw fa-headphones"></span>
+                        <span>Songs</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span class="fa fa-fw fa-music"></span>
+                        <span>Playlists</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span class="fa fa-fw fa-credit-card"></span>
+                        <span>Subscriptions</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span class="fa fa-fw fa-cogs"></span>
+                        <span>Settings</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="policies">
+            <ul>
+                <li>
+                    <a href="#">Cookies</a>
+                </li>
+                <li>
+                    <a href="#">Privacy</a>
+                </li>
+            </ul>
+        </div>
+    </div>`;
+    
+document.body.appendChild(sidebar.content);
